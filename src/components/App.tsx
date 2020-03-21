f42e844371ffd82d3d92afca62ea59c2ec2c64fa
ze <zelkatani@gmail.com>
2020-03-21 00:24:28 -0400

    Move key press handler up into App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,12 +5,25 @@ import Keyboard from "./Keyboard";
 export interface AppState {
   selected?: Set<number>;
   setSelected: (event: BoxEvent) => void;
+  data: Map<number, BoxData>;
 }
 
 export type BoxEvent =
   | { type: "add", box: number; }
   | { type: "set", box: number; }
   | { type: "clear"; };
+
+export type BoxData = {
+  marks: Set<number>;
+  value?: number;
+};
+
+export function getOrDefault(data: Map<number, BoxData>, id: number): BoxData {
+  return data.get(id) || {
+    value: undefined,
+    marks: new Set()
+  };
+}
 
 export default class App extends React.Component<{}, AppState> {
   setSelected = (event: BoxEvent) => {
@@ -33,16 +46,86 @@ export default class App extends React.Component<{}, AppState> {
     }
   };
 
-  state = {
+  state: AppState = {
     selected: undefined,
     setSelected: this.setSelected,
+    data: new Map()
   };
 
-  render() {
+  handleKeyDown = (event: KeyboardEvent) => {
     const { selected, setSelected } = this.state;
+    if (selected === undefined) return;
+
+    const { code, shiftKey } = event;
+
+    switch (code) {
+      case "Digit1":
+      case "Digit2":
+      case "Digit3":
+      case "Digit4":
+      case "Digit5":
+      case "Digit6":
+      case "Digit7":
+      case "Digit8":
+      case "Digit9": {
+        const str = code.charAt(code.length - 1);
+        const digit = Number.parseInt(str);
+
+        const data = new Map(this.state.data);
+        for (const id of selected) {
+          const { marks, value } = getOrDefault(data, id);
+
+          if (shiftKey) {
+            if (marks.has(digit)) {
+              marks.delete(digit);
+            } else {
+              marks.add(digit);
+            }
+
+            data.set(id, { marks, value });
+          } else {
+            data.set(id, { marks, value: digit });
+          }
+        }
+
+        this.setState({ data });
+        break;
+      }
+      case "Delete":
+      case "Backspace": {
+        const data = new Map(this.state.data);
+        for (const id of selected) {
+          const { marks, value } = getOrDefault(data, id);
+
+          if (shiftKey || !value) {
+            data.set(id, { marks: new Set(), value });
+          } else {
+            data.set(id, { marks, value: undefined });
+          }
+        }
+
+        this.setState({ data });
+        break;
+      }
+      case "Escape": {
+        setSelected({ type: "clear" });
+      }
+    }
+  };
+
+  componentDidMount() {
+    window.addEventListener("keydown", this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("keydown", this.handleKeyDown);
+  }
+
+  render() {
+    const { selected, setSelected, data } = this.state;
 
     return (<>
-      <Board selected={selected} setSelected={setSelected} />
+      <Board selected={selected} setSelected={setSelected} data={data} />
       <Keyboard isRegular={true} />
     </>);
   }
--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -1,102 +1,18 @@
 import React from "react";
-import { AppState } from "../App";
+import { BoxData, BoxEvent, getOrDefault } from "../App";
 import Box from "../Box";
 import "./index.scss";
 
-type BoardProps = AppState;
-
-type BoxData = {
-  marks: Set<number>;
-  value?: number;
-};
-
-interface BoardState {
+interface BoardProps {
+  selected?: Set<number>;
+  setSelected: (event: BoxEvent) => void;
   data: Map<number, BoxData>;
 }
 
-export default class Board extends React.Component<BoardProps, BoardState> {
-  state: BoardState = {
-    data: new Map()
-  };
-
-  getOrDefault(data: Map<number, BoxData>, id: number): BoxData {
-    return data.get(id) || {
-      value: undefined,
-      marks: new Set()
-    };
-  }
-
-  handleKeyDown = (event: KeyboardEvent) => {
-    const { selected, setSelected } = this.props;
-    if (selected === undefined) return;
-
-    const { code, shiftKey } = event;
-
-    switch (code) {
-      case "Digit1":
-      case "Digit2":
-      case "Digit3":
-      case "Digit4":
-      case "Digit5":
-      case "Digit6":
-      case "Digit7":
-      case "Digit8":
-      case "Digit9": {
-        const str = code.charAt(code.length - 1);
-        const digit = Number.parseInt(str);
-
-        const data = new Map(this.state.data);
-        for (const id of selected) {
-          const { marks, value } = this.getOrDefault(data, id);
-
-          if (shiftKey) {
-            if (marks.has(digit)) {
-              marks.delete(digit);
-            } else {
-              marks.add(digit);
-            }
-
-            data.set(id, { marks, value });
-          } else {
-            data.set(id, { marks, value: digit });
-          }
-        }
-
-        this.setState({ data });
-        break;
-      }
-      case "Delete":
-      case "Backspace": {
-        const data = new Map(this.state.data);
-        for (const id of selected) {
-          const { marks, value } = this.getOrDefault(data, id);
-
-          if (shiftKey || !value) {
-            data.set(id, { marks: new Set(), value });
-          } else {
-            data.set(id, { marks, value: undefined });
-          }
-        }
-
-        this.setState({ data });
-        break;
-      }
-      case "Escape": {
-        setSelected({ type: "clear" });
-      }
-    }
-  };
-
-  componentDidMount() {
-    window.addEventListener("keydown", this.handleKeyDown);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener("keydown", this.handleKeyDown);
-  }
+export default class Board extends React.Component<BoardProps> {
 
   render() {
-    const { selected, setSelected } = this.props;
+    const { selected, setSelected, data } = this.props;
 
     const boxes = [];
     for (let i = 0; i < 9; i++) {
@@ -107,7 +23,7 @@ export default class Board extends React.Component<BoardProps, BoardState> {
         const id = i * 9 + j;
         const isSelected = selected?.has(id) || false;
 
-        const { marks, value } = this.getOrDefault(this.state.data, id);
+        const { marks, value } = getOrDefault(data, id);
         boxes.push((<Box key={id}
           id={id}
           row={i}
